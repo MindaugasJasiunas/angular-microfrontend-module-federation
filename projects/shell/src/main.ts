6c72bbf3b7b952c67149ef62f85990e4dfce026b
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-10-06 16:42:19 +0300

    pre-load all remote entries on app startup #4

--- projects/shell/src/main.ts
+++ projects/shell/src/main.ts
@@ -1,2 +1,12 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+import { loadRemoteEntry } from '@angular-architects/module-federation';
+
+Promise.all([
+  loadRemoteEntry({
+    type: 'module',
+    remoteEntry: 'http://localhost:3000/remoteEntry.js',
+  }),
+])
+  .then(() => import('./bootstrap'))
+  .catch((err) => console.error(err));
+
+//import('./bootstrap').catch((err) => console.error(err));
